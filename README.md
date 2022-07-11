55904a6a83c9014327c55ffcb3c16ee95ab6cb5c
Srishty Mangutte <68679980+Srish-ty@users.noreply.github.com>
2022-07-12 05:18:01 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Watchstore_Site
+ A site for an e-commerce website of a watchShop
---
+++ jsfile.js
@@ -0,0 +1,18 @@
+
+function textout(){
+    document.getElementById("cart").innerHTML="Cart";
+}
+function normal(){
+    document.getElementById("cart").innerHTML="<img src='carti1.png'>";
+}
+
+const headis=document.getElementsByClassName("headings");
+//console.log(headis);
+let l=headis.length;
+//console.log(l);
+var i=0;
+for(i; i<l; i++){
+    var hed=headis[i]; var b=hed.innerHTML;
+    var c= b.toUpperCase;
+    hed.innerHTML=c;
+}
